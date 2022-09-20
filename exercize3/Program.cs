f62572f61b514ea8c63186148f96aea79e6a25d3
anna.s <baffy.009@mail.ru>
2022-09-21 00:27:28 +0300

    ex2 commit

---
+++ exercize3/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Console.WriteLine("Введите число:");
+string input1 = Console.ReadLine();
+int num1 = int.Parse(input1);
+if (num1 % 2==1)
+{
+    Console.WriteLine("Нет");       
+}
+else
+{
+    Console.WriteLine("Да");
+}
